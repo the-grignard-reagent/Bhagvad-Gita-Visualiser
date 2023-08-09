ccfa9ddcd3c08085e74d89727456fcd767e1ec91
Suraj <surajsharmapathak2018@gmail.com>
2023-08-09 18:36:09 +0530

    Change#001: Staged the initial files

---
+++ src/components/Body/apiConfig.jsx
@@ -0,0 +1,9 @@
+// apiConfig.js
+const host = 'bhagavad-gita3.p.rapidapi.com'
+; // Replace with your actual host URL
+const apiKey = 'c1a134b2a9mshdbe26baf67adeddp1b5f35jsnfb8664403786'; // Replace with your actual API key
+
+export const apiConfig = {
+  host,
+  apiKey,
+};
