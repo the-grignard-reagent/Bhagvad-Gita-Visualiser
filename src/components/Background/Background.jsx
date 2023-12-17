1da3eec66896663d3e63da08587be76ebe930168
the-grignard-reagent <76035351+the-grignard-reagent@users.noreply.github.com>
2023-12-17 19:07:03 +0530

    Change#004: Updated Background.jsx

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -1,13 +1,18 @@
+// Importing the necessary dependencies
 import React from "react";
 import Particles from "react-tsparticles";
-import particleConfig from "./particles";
+import particleConfig from "./particles"; // Importing the particle configuration from a separate file
 
+// Defining the Background component
 function Background() {
   return (
+    // Rendering a <div> to contain the particle animation
     <div>
+      {/* Rendering the Particles component with the specified particle configuration */}
       <Particles params={particleConfig} />
     </div>
   );
 }
 
+// Exporting the Background component as the default export
 export default Background;
