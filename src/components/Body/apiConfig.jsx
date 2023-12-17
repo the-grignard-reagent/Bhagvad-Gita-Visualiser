d4e6b2fead74d5ed4d339ad9f38dab9577bf15dc
the-grignard-reagent <76035351+the-grignard-reagent@users.noreply.github.com>
2023-12-17 19:10:32 +0530

    Change#007: Updated apiConfig.jsx

--- src/components/Body/apiConfig.jsx
+++ src/components/Body/apiConfig.jsx
@@ -1,8 +1,12 @@
 // apiConfig.js
-const host = 'bhagavad-gita3.p.rapidapi.com'
-; // Replace with your actual host URL
+
+// The host URL for the Bhagavad Gita API
+const host = 'bhagavad-gita3.p.rapidapi.com'; // Replace with your actual host URL
+
+// The API key for accessing the Bhagavad Gita API
 const apiKey = 'c1a134b2a9mshdbe26baf67adeddp1b5f35jsnfb8664403786'; // Replace with your actual API key
 
+// Exporting an object with host and apiKey for API configuration
 export const apiConfig = {
   host,
   apiKey,
